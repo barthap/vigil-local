546501279dfe41a9a27f7535778876fbb6fb82a5
Bartłomiej Klocek <bartlomiej.klocek@swmansion.com>
2025-07-07 21:26:22 +0200

    Fix config env substitution

--- src/config/reader.rs
+++ src/config/reader.rs
@@ -14,19 +14,52 @@ use crate::APP_ARGS;
 
 pub struct ConfigReader;
 
+/// Check whether `value` contains invalid characters.
+fn validate<S>(value: S, kind: &str) -> Result<(), String>
+where
+    S: AsRef<str>,
+{
+    static FORBIDDEN: &[&str] = &["$", "{", "}"];
+    for c in FORBIDDEN {
+        if value.as_ref().contains(c) {
+            let err_msg = format!(
+                "variable {} '{}' contains forbidden character '{}'",
+                kind,
+                value.as_ref(),
+                c
+            );
+            return Err(err_msg);
+        };
+    }
+    Ok(())
+}
+
 impl ConfigReader {
     pub fn make() -> Config {
         debug!("reading config file: {}", &APP_ARGS.config);
 
         // Read configuration
-        let mut conf = fs::read_to_string(&APP_ARGS.config).expect("cannot find config file");
+        let raw_conf = fs::read_to_string(&APP_ARGS.config).expect("cannot find config file");
 
         debug!("read config file: {}", &APP_ARGS.config);
 
         // Replace environment variables
-        let environment = env::vars().collect::<HashMap<String, String>>();
+        let environment = env::vars()
+            .filter(|(k, v)| {
+                if let Err(err) = validate(k, "key") {
+                    warn!("Skipped invalid env var '{k}': {err}");
+                    return false;
+                }
+                if let Err(err) = validate(v, "value") {
+                    warn!("Skipped invalid env var '{k}': {err}");
+                    return false;
+                }
+                true
+            })
+            .collect::<HashMap<String, String>>();
 
-        // conf = substitute(&conf, &environment).expect("cannot substitute environment variables");
+        let conf =
+            substitute(&raw_conf, &environment).expect("cannot substitute environment variables");
 
         // Parse configuration
         toml::from_str(&conf).expect("syntax error in config file")
